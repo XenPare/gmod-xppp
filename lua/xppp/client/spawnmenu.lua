3983d2ef197dfb15eb8dfae8a1c9f6d9bbb89a09
crester <35332822+cresterienvogel@users.noreply.github.com>
2022-07-07 16:24:58 +0700

    tabs

--- lua/xppp/client/spawnmenu.lua
+++ lua/xppp/client/spawnmenu.lua
@@ -2,13 +2,13 @@ local tag = "XPPP Spawnmenu"
 local cfg = XPPP.CFG
 
 hook.Add("PopulateContent", tag, function()
-    for k, v in pairs(spawnmenu.GetPropTable()) do
-        for _k, _v in pairs(v.contents) do
+	for k, v in pairs(spawnmenu.GetPropTable()) do
+		for _k, _v in pairs(v.contents) do
 			if _v.model and (XPPP.BlockedModels[_v.model:lower()] or cfg.CustomBlockedModels[_v.model:lower()]) then
 				spawnmenu.GetPropTable()[k].contents[_k] = nil
 			end
-        end
-    end
+		end
+	end
 end)
 
 hook.Add("PreReloadToolsMenu", tag, function()
